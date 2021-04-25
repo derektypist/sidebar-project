d590b730b1ee8ef0a3df6eb34c51bdab2fb4a9e4
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-25 15:30:53 +0000

    Added constants at script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Set Up Constants
+
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+// Add Event Listeners
